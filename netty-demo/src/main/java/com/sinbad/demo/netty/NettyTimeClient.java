43ed4c18d0417c8f7dca31ccb4c3776c830618ed
szw-linux <sunzhiwei1@xiaomi.com>
2020-09-13 14:30:47 +0800

    init

---
+++ netty-demo/src/main/java/com/sinbad/demo/netty/NettyTimeClient.java
@@ -0,0 +1,4 @@
+package com.sinbad.demo.netty;
+
+public class NettyTimeClient {
+}
---
+++ netty-demo/src/main/java/com/sinbad/demo/netty/NettyTimeServiceHandler.java
@@ -0,0 +1,6 @@
+package com.sinbad.demo.netty;
+
+import io.netty.channel.ChannelHandlerAdapter;
+
+public class NettyTimeServiceHandler extends ChannelHandlerAdapter {
+}
